6aff5e6e9d8e5cafd062fbdbdec029b76da7567d
Thiago Garcia <thiagogds14@gmail.com>
2016-05-16 11:58:19 -0300

    Add setup file

---
+++ gate_id/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), '../README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='gate-id',
+    version='1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Django app integraton with Gate ID SSO',
+    long_description=README,
+    author='Thiago Garcia',
+    author_email='thiagogds14@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',  # replace "X.Y" as appropriate
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
