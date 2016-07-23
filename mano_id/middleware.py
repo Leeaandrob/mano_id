3eea71e25a0db9e2bfae30dca901801d64d2e8d7
Leandro Barbosa <leandrobar93@gmail.com>
2016-07-23 17:05:58 -0300

    update

--- mano_id/middleware.py
+++ mano_id/middleware.py
@@ -1,31 +1,33 @@
 import requests
-from unipath import Path
 
-from django.contrib.auth import authenticate, login, get_user_model
-from django.utils.functional import SimpleLazyObject
+from django.contrib.auth import login, get_user_model
 
-from gate_id import settings
+from mano_id import settings
+
 
 User = get_user_model()
 
-class GateSessionMiddleware(object):
+
+class ManoSessionMiddleware(object):
 
     def process_request(self, request):
         if request.user.is_anonymous():
-            gate_id = request.COOKIES.get(settings.GATE_COOKIE_NAME)
-            gate_user = requests.get(
-                settings.GATE_ID_URL,
-                cookies = {settings.GATE_COOKIE_NAME: gate_id}
+            mano_id = request.COOKIES.get(settings.MANO_COOKIE_NAME)
+            mano_user = requests.get(
+                settings.MANO_ID_URL,
+                cookies={settings.MANO_COOKIE_NAME: mano_id}
             ).json()
 
-            if gate_user:
+            if mano_user:
                 try:
-                    user = User.objects.get(email=gate_user['email'], uuid=gate_user['uuid'])
+                    user = User.objects.get(email=mano_user['email'],
+                                            uuid=mano_user['uuid'])
                 except User.DoesNotExist:
-                    user = User.objects.create_user(gate_user['email'], uuid=gate_user['uuid'])
+                    user = User.objects.create_user(mano_user['email'],
+                                                    uuid=mano_user['uuid'])
 
-                user.first_name = gate_user['first_name']
-                user.last_name = gate_user['last_name']
+                user.first_name = mano_user['first_name']
+                user.last_name = mano_user['last_name']
                 user.save()
 
                 user.backend = 'django.contrib.auth.backend.ModelBackend'
